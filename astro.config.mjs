59e64516e502e6442a9cd4a91db77f60d8cdec18
Susan Silver <hello@susansilver.net>
2024-05-25 11:47:20 -0700

    add components and images from previous version

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://beautyofmathematics.com',
   integrations: [
     tailwind(),
   ]
---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Beauty of Mathematics';
+export const SITE_DESCRIPTION = 'Discover one persons journey and triumph over math anxiety. Learn about their personal struggles, the strategies they used, and the lessons they learned along the way to overcome math stress.';
